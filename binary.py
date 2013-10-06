f4d2d409e97acefd215b69e1282b9e9c2aad5e9e
Victor van den Elzen <victor.vde@gmail.com>
2013-10-06 03:02:39 +0200

    binary stuff

--- binary.py
+++ binary.py
@@ -1,4 +1,5 @@
-from struct import unpack, calcsize
+from struct import pack, unpack, calcsize
+from collections import OrderedDict
 
 def getbytes(s, n):
     b = s.read(n)
@@ -8,20 +9,219 @@ def getbytes(s, n):
 def getbyte(s):
     return getbytes(s, 1)
 
-def getstring(s, delimiter):
-    lc = []
-    c = getbyte(s)
-    while c != delimiter:
-        lc.append(c)
+class BaseField(object):
+    def unpack(self, s):
+        raise NotImplementedError
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, v):
+        return self._data
+
+class Struct(BaseField):
+    def __init__(self):
+        self.given_data = None
+        self.stream = None
+
+    def add_field(self, name, f):
+        assert name not in self.field, name
+        self.field[name] = f
+        if self.given_data:
+            f.data = self.given_data.get(name, None)
+        elif self.stream:
+            f.unpack(self.stream)
+        else:
+            assert False, "no data or stream"
+
+    def unpack(self, s):
+        self.field = OrderedDict()
+        self.stream = s
+        self.fields()
+        self.stream = None
+
+    @property
+    def data(self):
+        data = OrderedDict()
+        for k, v in self.field.iteritems():
+            data[k] = v.data
+        return data
+
+    @data.setter
+    def data(self, v):
+        self.field = OrderedDict()
+        self.given_data = v
+        self.fields()
+        self.given_data = None
+
+    def fields(self):
+        raise NotImplementedError
+
+class MagicField(BaseField):
+    def __init__(self, magic):
+        self.magic = magic
+
+    def unpack(self, s):
+        data = getbytes(s, len(self.magic))
+        assert data == self.magic
+        return
+
+    @property
+    def data(self):
+        return self.magic
+
+    @data.setter
+    def data(self, v):
+        assert v == self.magic or v is None, v
+
+class FormatField(BaseField):
+    def __init__(self, fmt):
+        if fmt[0] in "@=<>!":
+            bosa = fmt[0]
+            fmt = fmt[1:]
+        else:
+            bosa = "<"
+        self.bosa = bosa
+        self.fmt = fmt
+        self.single = len(fmt) == 1
+
+    def unpack(self, s):
+        fmt = self.bosa + self.fmt
+        size = calcsize(fmt)
+        lc = s.read(size)
+        assert len(lc) == size, "Unexpected EOF"
+        self._data = unpack(fmt, lc)
+
+    @property
+    def data(self):
+        if self.single == 1:
+            assert len(self._data) == 1
+            return self._data[0]
+        else:
+            return self._data
+
+    @data.setter
+    def data(self, v):
+        if self.single:
+            self._data = (v,)
+        else:
+            self._data = v
+
+class BaseArrayField(BaseField):
+    def __init__(self, field_function=None, indexed_function=None):
+        if indexed_function is None:
+            indexed_function = lambda i: field_function()
+        self.field_fun = indexed_function
+
+    def array_size(self):
+        raise NotImplementedError
+
+    def unpack(self, s):
+        self.field = [self.field_fun(i) for i in xrange(self.array_size())]
+        for f in self.field:
+            f.unpack(s)
+
+    @property
+    def data(self):
+        return [f.data for f in self.field]
+
+    @data.setter
+    def data(self, v):
+        self.field = [self.field_fun(i) for i in xrange(self.array_size())]
+        for f, fv in zip(self.field, v):
+            f.data = fv
+
+class ArrayField(BaseArrayField):
+    def __init__(self, size, *args, **kwargs):
+        self.size = size
+        BaseArrayField.__init__(self, *args, **kwargs)
+
+    def array_size(self):
+        return self.size
+
+class DependentArrayField(BaseArrayField):
+    def __init__(self, prefix_field, *args, **kwargs):
+        self.prefix_field = prefix_field
+        BaseArrayField.__init__(self, *args, **kwargs)
+
+    def array_size(self):
+        return self.prefix_field.data
+
+    @BaseArrayField.data.setter
+    def data(self, v):
+        self.prefix_field.data = len(v)
+        BaseArrayField.data.__set__(v)
+
+class PrefixedArrayField(DependentArrayField):
+    def unpack(self, s):
+        self.prefix_field.unpack(s)
+        DependentArrayField.unpack(self, s)
+
+class BlobField(BaseField):
+    def __init__(self, size):
+        self.size = size
+
+    def unpack(self, s):
+        self._data = getbytes(s, self.size)
+
+class DependentBlobField(BaseField):
+    def __init__(self, prefix_field):
+        self.prefix_field = prefix_field
+
+    def unpack(self, s):
+        self._data = getbytes(s, self.prefix_field.data)
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, v):
+        self.prefix_field.data = len(v)
+        self._data = v
+
+class PrefixedBlobField(DependentBlobField):
+    def unpack(self, s):
+        self.prefix_field.unpack(s)
+        DependentBlobField.unpack(self, s)
+
+class StringField(BaseField):
+    def unpack(self, s):
+        lc = []
         c = getbyte(s)
-    return ''.join(lc)
+        while c != "\0":
+            lc.append(c)
+            c = getbyte(s)
+        self._data = "".join(lc)
 
-def getunpack(s, fmt):
-    size = calcsize(fmt)
-    lc = s.read(size)
-    assert len(lc) == size, "Unexpected EOF"
-    return unpack(fmt, lc)
+class IndexField(FormatField):
+    def __init__(self, array, *args, **kwargs):
+        self.array = array
+        FormatField.__init__(self, *args, **kwargs)
 
-def skipbytes(s, bs):
-    b = getbytes(s, len(bs))
-    assert b == bs
+    @property
+    def data(self):
+        return self.array[FormatField.data.__get__(self)]
+
+    @data.setter
+    def data(self, v):
+        FormatField.data.__set__(self, self.array.index(v))
+
+def fieldmaker(field):
+    def maker(i, name, *args, **kwargs):
+        f = field(*args, **kwargs)
+        i.add_field(name, f)
+    return maker
+
+Magic = fieldmaker(MagicField)
+Format = fieldmaker(FormatField)
+Array = fieldmaker(ArrayField)
+DependentArray = fieldmaker(DependentArrayField)
+PrefixedArray = fieldmaker(PrefixedArrayField)
+String = fieldmaker(StringField)
+Blob = fieldmaker(BlobField)
+DependentBlob = fieldmaker(DependentBlobField)
+PrefixedBlob = fieldmaker(PrefixedBlobField)
+Index = fieldmaker(IndexField)
--- parse_pcf.py
+++ parse_pcf.py
@@ -1,89 +1,72 @@
-from binary import getbytes, skipbytes, getstring, getunpack
+from binary import Struct, Magic, Format, FormatField, StringField, BlobField, PrefixedBlobField, PrefixedArray, PrefixedArrayField, DependentArray, DependentArrayField, Index
+import json
+from uuid import UUID
 
-class Element(object):
-    def __init__(self, type, name, guid):
-        self.type = type
-        self.name = name
-        self.guid = guid
-        self.attributes = []
+attribute_types = {
+    1 : lambda: FormatField("I"), # element index
+    2 : lambda: FormatField("I"), # integer
+    3 : lambda: FormatField("f"), # float
+    4 : lambda: FormatField("?"), # bool
+    5 : lambda: FormatField("I"), # string
+    6 : lambda: PrefixedBlobField(FormatField("I")), # blob
+    7 : lambda: FormatField("I"), # time
+    8 : lambda: FormatField("4B"), # color
+    9 : lambda: FormatField("2f"), # vector2
+    10 : lambda: FormatField("3f"), # vector3
+    11 : lambda: FormatField("4f"), # vector4
+    12 : lambda: FormatField("3f"), # angle
+    13 : lambda: FormatField("4f"), # quaternion
+    14 : lambda: FormatField("16f"), # matrix
+    }
 
-class Attribute(object):
-    def __init__(self, name, type, data):
-        self.name = name
-        self.type = type
-        self.data = data
+class UUIDField(BlobField):
+    def __init__(self):
+        BlobField.__init__(self, 16)
+
+    @property
+    def data(self):
+        return UUID(bytes=self._data).urn
+
+    @data.setter
+    def data(self, v):
+        self._data = UUID(v).bytes
+
+class Attribute(Struct):
+    def __init__(self, strings):
+        self.strings = strings
+        Struct.__init__(self)
+
+    def fields(self):
+        Index(self, "name", self.strings, "I")
+        Format(self, "type", "B")
+        type = self.data["type"]
+        if 1 <= type <= 14:
+            self.add_field("data", attribute_types[type]())
+        elif 14 < type <= 28:
+            PrefixedArray(self, "data", FormatField("I"), attribute_types[type - 14])
+        else:
+            assert False, type
+
+class Element(Struct):
+    def __init__(self, strings):
+        self.strings = strings
+        Struct.__init__(self)
+
+    def fields(self):
+        Index(self, "type", self.strings, "I")
+        Index(self, "name", self.strings, "I")
+        self.add_field("guid", UUIDField())
+
+class PCF(Struct):
+    def fields(self):
+        Magic(self, "magic", "<!-- dmx encoding binary 5 format pcf 2 -->\n\0")
+        PrefixedArray(self, "strings", FormatField("I"), StringField)
+        strings = self.field["strings"].data
+        Format(self, "nelements", "I")
+        DependentArray(self, "elements", self.field["nelements"], lambda: Element(strings))
+        DependentArray(self, "attributes", self.field["nelements"], lambda: PrefixedArrayField(FormatField("I"), lambda: Attribute(strings)))
 
 with open("x.pcf", "rb") as s:
-    skipbytes(s, "<!-- dmx encoding binary 5 format pcf 2 -->\n\0")
-    #skipbytes(s, "<!-- dmx encoding binary 5 format dmx 1 -->\n\0")
-    (nstrings,) = getunpack(s, "<I")
-    strings = []
-    for i in xrange(nstrings):
-        strings.append(getstring(s, '\0'))
-
-    def gettablestring():
-        (index,) = getunpack(s, "<I")
-        return strings[index]
-
-    (nelements,) = getunpack(s, "<I")
-    elements = []
-    for i in xrange(nelements):
-        e_type = gettablestring()
-        e_name = gettablestring()
-        e_guid = getunpack(s, "<16B")
-        elements.append(Element(e_type, e_name, e_guid))
-
-    def get_Element():
-        return getunpack(s, "<I")[0]
-    def get_int():
-        return getunpack(s, "<I")[0]
-    def get_float():
-        return getunpack(s, "<f")[0]
-    def get_bool():
-        return getunpack(s, "<?")[0]
-    def get_str():
-        return gettablestring()
-    def get_Binary():
-        size = getunpack(s, "<I")[0]
-        return getbytes(size)
-    def get_Time():
-        return getunpack(s, "<I")[0]
-    def get_Color():
-        return getunpack(s, "<4B")
-    def get_Vector2():
-        return getunpack(s, "<2f")
-    def get_Vector3():
-        return getunpack(s, "<3f")
-    def get_Vector4():
-        return getunpack(s, "<4f")
-    def get_Angle():
-        return getunpack(s, "<3f")
-    def get_Quaternion():
-        return getunpack(s, "<4f")
-    def get_Matrix():
-        return getunpack(s, "<16f")
-    attribute_types = [get_Element, get_int, get_float, get_bool, get_str, get_Binary, get_Time, get_Color, get_Vector2, get_Vector3, get_Vector4, get_Angle, get_Quaternion, get_Matrix]
-
-    for i in xrange(nelements):
-        (nattributes,) = getunpack(s, "<I")
-        attributes = []
-        for j in xrange(nattributes):
-            a_name = gettablestring()
-            (a_type,) = getunpack(s, "<B")
-            a_type -= 1
-            if a_type >= len(attribute_types):
-                a_type -= len(attribute_types)
-                (size,) = getunpack(s, "<I")
-                a_data = []
-                for k in xrange(size):
-                    a_data.append(attribute_types[a_type]())
-            else:
-                a_data = attribute_types[a_type]()
-            elements[i].attributes.append(Attribute(a_name, a_type, a_data))
-
-    for i in xrange(len(elements)):
-        e = elements[i]
-        print u'Element {} "{}" type {}'.format(i, e.name, e.type, e.guid)
-        for a in e.attributes:
-            print u'\t"{}"  type {} data {}'.format(a.name, a.type, repr(a.data))
-
+    p = PCF()
+    p.unpack(s)
+    print(json.dumps(p.data, indent=4))
