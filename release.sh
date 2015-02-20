63d70441fce5587e6a31f5c30de7404ca8b71fc2
Victor van den Elzen <victor.vde@gmail.com>
2015-02-20 01:06:34 +0100

    two log files is one too many

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ rm -r dota || true
 fusermount -u ~/dota_unpacked || true
 ~/unvpk/build/vpkfs/vpkfs "/mnt/steam/steamapps/common/dota 2 beta/dota/pak01_dir.vpk" ~/dota_unpacked
 # ~/unvpk/build/vpkfs/vpkfs "/mnt/steam/steamapps/common/dota 2 test/dota/pak01_dir.vpk" ~/dota_unpacked
-python3 -u nohats.py ~/dota_unpacked dota2_nohats "$@" > nohats_log.txt 2> nohats_warnings.txt
+python3 -u nohats.py ~/dota_unpacked dota2_nohats "$@" > nohats_log.txt 2>&1
 
 unix2dos -n README.md readme.txt
 
@@ -23,4 +23,4 @@ python3 vpk.py a nohats/pak01 dota2_nohats/
 mkdir dota
 cp gameinfo.txt dota/gameinfo.txt
 
-7z a -mx -ms=off dota2_nohats.7z nohats dota nohats_log.txt nohats_warnings.txt readme.txt > /dev/null
+7z a -mx -ms=off dota2_nohats.7z nohats dota nohats_log.txt readme.txt > /dev/null
